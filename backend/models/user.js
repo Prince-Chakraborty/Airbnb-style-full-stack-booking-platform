b62990c5cd337cc32ae7600e4dfd508ec8511c03
Prince Chakraborty <princechakraborty@Princes-MacBook-Air.local>
2026-02-08 01:20:28 +0530

    Fix user model and update package.json

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,9 +6,14 @@ const userSchema = new Schema({
     email: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        enum: ["guest", "host"],
+        default: "guest"
     }
 });
 
 userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
